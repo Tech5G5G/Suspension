5c57484e16970f1243735c7e87f0282e45d4e94f
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2025-06-30 17:17:40 -0700

    Add BaseMapLayer setting

--- Suspension/Settings/SettingValues.cs
+++ Suspension/Settings/SettingValues.cs
@@ -3,4 +3,6 @@
 public static class SettingValues
 {
     public static Setting<bool> StatusBar { get; } = new(nameof(StatusBar), true);
+
+    public static Setting<string> BaseMapLayer { get; } = new(nameof(BaseMapLayer), "https://mts1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}");
 }
