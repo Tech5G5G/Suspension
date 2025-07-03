3edbce12659fc7c5f0b32660de57e46947448efc
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2025-07-03 14:25:42 -0700

    Add setting for toggling airtime annotations

--- Suspension/Settings/SettingValues.cs
+++ Suspension/Settings/SettingValues.cs
@@ -4,5 +4,7 @@ public static class SettingValues
 {
     public static Setting<bool> StatusBar { get; } = new(nameof(StatusBar), true);
 
+    public static Setting<bool> Airtimes { get; } = new(nameof(Airtimes), false);
+
     public static Setting<string> BaseMapLayer { get; } = new(nameof(BaseMapLayer), "https://mts1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}");
 }
