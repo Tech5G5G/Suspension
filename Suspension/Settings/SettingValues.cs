243e8526413926ff128f98007afc30cb46c7fc02
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2025-08-12 22:33:16 -0700

    Remove Gemini API key setting

--- Suspension/Settings/SettingValues.cs
+++ Suspension/Settings/SettingValues.cs
@@ -5,6 +5,4 @@ public static class SettingValues
     public static Setting<bool> StatusBar { get; } = new(nameof(StatusBar), true);
 
     public static Setting<bool> Airtimes { get; } = new(nameof(Airtimes), false);
-
-    public static Setting<string> GeminiAPIKey { get; } = new(nameof(GeminiAPIKey), string.Empty);
 }
